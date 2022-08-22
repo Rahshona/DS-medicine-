25f512d4b4c9e97445da57f4250eb633136860fd
Rakhshona <rahshonakarimova@gmail.com>
2022-08-22 16:25:09 +0500

    Lesson 5 Task 1

--- Домашка урока 5/L5_hw_Task1.py
+++ Домашка урока 5/L5_hw_Task1.py
@@ -1,5 +1,5 @@
-a = print('Enter everything what you want or empty space to end:\n')
 Task_1 = open('text_1.txt', 'w', encoding='utf-8')
+a = print('Enter everything what you want or empty space to end:\n')
 while (a := input()) != '':
     Task_1.write(f'{a}\n')
 
