a7e71655dec6d5a421a11df8fe386ef6dfdbe007
Rakhshona <rahshonakarimova@gmail.com>
2022-10-13 23:48:55 +0500

    Lesson 7 Task2

--- Домашка урока 7/L7Task2.py
+++ Домашка урока 7/L7Task2.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class Clothes():
 
     def __init__(self, width, height):
@@ -13,6 +14,7 @@ class Clothes():
     def __str__(self):
         return str(self.width, self.height)
 
+
 class Coat(Clothes):
 
     @property
@@ -25,6 +27,7 @@ class Suit(Clothes):
     @property
     def result(self):
         return round(self.height * 2 + 0.3)
+
 
 coat = Coat(46, 1.75)
 suit = Suit(65, 1.65)
